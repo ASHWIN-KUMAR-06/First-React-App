a264b8e652fa263275a8aef153b24e6f6d1ca3c3
ASHWIN KUMAR <ashwinchauhanmr360@gmail.com>
2022-09-22 08:52:08 +0530

    Updates

---
+++ precache-manifest.c4fa6586333ed846a300068af0a1f9a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1da5cebedb3e39d3af60",
+    "url": "/First-React-App/static/css/main.18bf5728.chunk.css"
+  },
+  {
+    "revision": "1da5cebedb3e39d3af60",
+    "url": "/First-React-App/static/js/main.1da5cebe.chunk.js"
+  },
+  {
+    "revision": "94421d0c55535ff39cc4",
+    "url": "/First-React-App/static/js/1.94421d0c.chunk.js"
+  },
+  {
+    "revision": "a6598096b981c8b2c345",
+    "url": "/First-React-App/static/js/2.a6598096.chunk.js"
+  },
+  {
+    "revision": "bd8faa56827b72d0f878",
+    "url": "/First-React-App/static/js/runtime~main.bd8faa56.js"
+  },
+  {
+    "revision": "d7160a5c4f97579d7b4aaae84bf62f31",
+    "url": "/First-React-App/static/media/homepage.d7160a5c.svg"
+  },
+  {
+    "revision": "e9b46a2a76f9727640cfc3267b0127f3",
+    "url": "/First-React-App/static/media/about.e9b46a2a.svg"
+  },
+  {
+    "revision": "f405b26a9acd4f7849420912732c60f0",
+    "url": "/First-React-App/index.html"
+  }
+];
